b55d6ce3c1869529c71a51558b70e26f5a4dec87
Javier Gonzalez Escortell <Javier.Gonzalez-Escortell@gft.com>
2022-04-25 16:06:46 +0200

    Added config and bean initialization example

--- src/main/java/guru/springframework/spring5jokesappv2/services/ChuckNorrisJokeServiceImpl.java
+++ src/main/java/guru/springframework/spring5jokesappv2/services/ChuckNorrisJokeServiceImpl.java
@@ -1,15 +1,18 @@
 package guru.springframework.spring5jokesappv2.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChuckNorrisJokeServiceImpl implements JokeService{
-    private final ChuckNorrisQuotes chuckNorrisQuotes;
+    @Autowired
+    private ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public ChuckNorrisJokeServiceImpl() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
-    }
+    //public ChuckNorrisJokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+    //    this.chuckNorrisQuotes = chuckNorrisQuotes;
+    //}
+
     @Override
     public String getJoke(){
         return chuckNorrisQuotes.getRandomQuote();
